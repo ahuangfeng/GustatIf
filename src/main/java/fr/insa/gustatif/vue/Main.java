d4a1efc6ca7236ca543b2feefa29f73b3f945226
totorigolo <toto.rigolo@free.fr>
2017-03-06 15:04:00 +0100

    Erreurs corrigées, projet configuré (normalement...)

--- src/main/java/fr/insa/gustatif/vue/Main.java
+++ src/main/java/fr/insa/gustatif/vue/Main.java
@@ -1,11 +1,19 @@
 package fr.insa.gustatif.vue;
 
+import fr.insa.gustatif.dao.JpaUtil;
+
 /**
  *
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola.");
+        System.out.println("Hola amigos !");
+        
+        JpaUtil.init();
+        
+        // ...
+        
+        JpaUtil.destroy();
     }
 }
